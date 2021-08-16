f95ae7069a7a9bdf54c1ae2b442c3327bfb463a8
juan pablo <juanpablo@MacBook-Pro-de-juan.local>
2021-08-16 17:26:33 -0500

    endpoints

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,16 +1,10 @@
+require 'byebug'
 module Api
   module V1
-    class LinksController < Api::V1::ApiController  
+    class LinksController < Api::V1::ApiController
+      before_action :set_link, only: %i[update destroy]
       before_action do
         permission?(Link)
-      end
-      def index
-        link = Link.where(user_id: current_user.id)
-        render json: link
-      end
-      def show
-        link = Link.find_by(lookup_code: params[:id])
-        render json: link
       end
 
       def create
@@ -24,13 +18,40 @@ module Api
           render json: error_message(@link)
         end
       end
-    
-      private 
-      def link_params
-        params.require(:link).permit(:original_url, :title).merge(user_id: current_user.id)
+
+      def index
+        link = Link.all
+        render json: link
       end
+
+      def show
+        link = Link.find_by(lookup_code: params[:id])
+        render json: link
+      end
+
+      def update
+        if @link.update(link_params)
+          render json: @link
+        else
+          render json: error_messages(@link)
+        end
+      end
+
+      def destroy
+        @link.destroy
+       if @link.destroy
+        render json: { message: "removed" }, status: :ok
+       end
+      end
+
+      private
+
       def set_link
         @link = Link.find(params[:id])
+      end
+
+      def link_params
+        params.require(:link).permit(:original_url, :title).merge(user_id: current_user.id)
       end
     end
   end
