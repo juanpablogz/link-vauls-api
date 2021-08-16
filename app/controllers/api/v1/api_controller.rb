7492da3993b43519537046d14d9106a413c7a03f
juan pablo <juanpablo@MacBook-Pro-de-juan.local>
2021-08-16 13:23:11 -0500

    xx

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -8,7 +8,7 @@ module Api
       # after_action :verify_policy_scoped, only: :index
 
       before_action :authenticate_user!, except: :status
-      # skip_after_action :verify_authorized, only: :status
+      skip_after_action :verify_authorized, only: :status
 
       rescue_from ActiveRecord::RecordNotFound,        with: :render_not_found
       rescue_from ActiveRecord::RecordInvalid,         with: :render_record_invalid
